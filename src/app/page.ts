70c1825a0f5802886a68bc6cbffb28085f138ad4
Sander Dahlberg <sander.dahlberg@outlook.com>
2018-12-29 10:18:55 +0100

    removed unused fields

--- src/app/page.ts
+++ src/app/page.ts
@@ -3,14 +3,9 @@ import {Sort} from './sort';
 
 export interface Page<T> {
   content: T[];
-  empty: boolean;
-  first: boolean;
-  last: boolean;
-  number: number;
   numberOfElements: number;
   pageable: Pageable;
   size: number;
   sort: Sort;
   totalElements: number;
-  totalPages: number;
 }
--- src/app/sort.ts
+++ src/app/sort.ts
@@ -1,8 +1,5 @@
 import {Order} from './order';
 
 export interface Sort {
-  empty?: boolean;
-  sorted?: boolean;
-  unsorted?: boolean;
   orders: Order[];
 }
